364ee5eba0861037f2e6fa9dce7e8ed59c2d96a7
Harsh Bhasin <harshbhasin1403@gmail.com>
2025-11-02 23:50:00 +0530

    Add files via upload

---
+++ Linked List/L_206_E.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev= NULL, *curr = head, *forw  = NULL;
+        if(head==NULL){
+            return head;
+        }
+        while(curr->next!=NULL){
+            forw = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forw;
+            forw = forw->next;
+        }
+            curr->next = prev;
+            return curr;
+        
+    }
+};
